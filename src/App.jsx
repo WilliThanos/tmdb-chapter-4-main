924b37a2554e69b394d8a48cc2f6ccfe75c404bb
Syaiful Rizal Sidiq <127954312+rizal504@users.noreply.github.com>
2024-03-25 23:42:47 +0700

    perbaruan-2-movie popular

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
+import MoviePopuler from "./Rizal/MoviePopuler.jsx"
+
 
 export default function App() {
   const router = createBrowserRouter([
+    // {
+    //   path: "/",
+    //   element: <Home />,
+    // },
     {
       path: "/",
-      element: <Home />,
+      element: <MoviePopuler />,
     },
   ]);
 
