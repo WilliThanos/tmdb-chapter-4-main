faff8fbd0ac831db39f475cf6be05e7b139c3902
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-03-26 00:09:05 +0700

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Home";
-import NowPlaying from "./Farrel/NowPlaying.jsx";
 
 export default function App() {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <Home />,
-    },
-    {
-      path: "/Np",
-      element: <NowPlaying />,
     },
   ]);
 
