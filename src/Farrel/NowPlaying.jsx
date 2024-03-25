f8ad7c19a79e01c63027ccbdbfff4cf1581911e5
Farrelll <137156152+Farrelx9@users.noreply.github.com>
2024-03-25 17:46:34 +0700

    change export

--- src/Farrel/NowPlaying.jsx
+++ src/Farrel/NowPlaying.jsx
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
-function index() {
+const API_KEY = process?.env.API_KEY;
 
+const NowPlaying = () => {
+  const [nowPlaying, setNowPlaying] = useState([]);
+  //   console.log(process?.env.API_KEY);
+  useEffect(() => {
+    nowPlaying();
+  }, []);
 
+  const nowPlaying = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}&language=en-US&page=1&region=EN`,
+        { headers: { accept: "application/json" } }
+      );
+      console.log("response.data", response.data);
+      setNowPlaying(response.data.results);
+    } catch (error) {
+      console.error("Error Fetching Data: ", error);
+    }
+  };
 
   return (
-    <div>index</div>
-  )
-}
+    <div>
+      <div>index</div>;
+    </div>
+  );
+};
 
-export default index
+export default index;
