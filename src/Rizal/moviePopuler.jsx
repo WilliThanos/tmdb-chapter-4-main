0a04a9c678f0058a35ae460abbf6ebc39f413fdd
Syaiful Rizal Sidiq <127954312+rizal504@users.noreply.github.com>
2024-03-26 13:11:15 +0700

    updated 2

--- src/Rizal/moviePopuler.jsx
+++ src/Rizal/moviePopuler.jsx
@@ -32,10 +32,10 @@ export default function MoviePopuler() {
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div>
-          <div className="flex justify-between bg-[#1966e3] p-5 shadow-md">
-            <div className="font-bold text-2xl">Movie Area.com</div>
-            <div className="flex justify-center">
+        <div className="  bg-gradient-to-t from-blue-800  to-black">
+          <div className="flex justify-between bg-black p-5 shadow-md shadow-white">
+            <div className="font-bold text-2xl text-white">Movie Area</div>
+            <div className="flex justify-center text-white">
               <a
                 href=""
                 className="pr-5 text-xl font-semibold hover:text-white hover:bg-[#ffd230] px-2 py-1 rounded-full transition-colors duration-300 flex justify-center items-center"
@@ -56,36 +56,37 @@ export default function MoviePopuler() {
               </a>
             </div>
           </div>
-          <p className="text-5xl font-bold flex justify-center p-5">
+          <p className="text-5xl font-bold flex justify-center p-5 text-white">
             Movie Popular
           </p>
-          <div className="grid grid-cols-4 gap-5 py-5">
+          <div className="grid grid-cols-4 gap-5 py-5 px-5">
             {data.map((movie) => (
               <div
                 key={movie.id}
-                className="relative bg-white shadow-xl rounded-lg p-4"
+                className="relative bg-white shadow-xl rounded-xl "
               >
-                <div className="mt-2 flex justify-center items-center">
+                <div className="flex justify-center items-center">
                   <img
-                    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                     alt={movie.title}
                     className="rounded-xl shadow-md"
                   />
                 </div>
-
-                <h2 className="text-2xl font-semibold flex justify-center py-2">
-                  {movie.title}
-                </h2>
-                <h2 className="text-xl flex justify-center">
-                  {"Tanggal Rilis : " + " "}
-                  {movie.release_date}
-                </h2>
-                <p className="text-xl text-justify py-2 ">{movie.overview}</p>
-                <div className="py-10"></div>
-                <div className="absolute right-0 bottom-0 mb-4 mr-4 ">
-                  <button className=" bg-[#ffd230] text-xl py-2 px-4 border rounded-md hover:bg-blue-500 hover:text-white transition-colors duration-300">
-                    Detail Film
-                  </button>
+                <div className="p-4">
+                  <h2 className="text-2xl font-bold flex justify-center py-2">
+                    {movie.title}
+                  </h2>
+                  <h2 className="text-xl flex justify-center">
+                    {"Tanggal Rilis : " + " "}
+                    {movie.release_date}
+                  </h2>
+                  <p className="text-xl text-justify py-2 ">{movie.overview}</p>
+                  <div className="py-10"></div>
+                  <div className="absolute right-0 bottom-0 mb-4 mr-4 ">
+                    <button className=" bg-[#ffd230] text-xl py-2 px-4 border rounded-md hover:bg-blue-500 hover:text-white transition-colors duration-300">
+                      Detail Film
+                    </button>
+                  </div>
                 </div>
               </div>
             ))}
